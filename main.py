565547d3822b859d82c6ac842efa279a20fed2f2
05443 <0544354125m@gmail.com>
2022-08-31 12:31:49 +0300

    git init

---
+++ main.py
@@ -0,0 +1,30 @@
+from email.message import EmailMessage
+from app2 import password
+import ssl
+import smtplib
+
+
+email_sender = '0544354125m@gmail.com'
+email_password = password
+
+email_reciver = '0544354125m@gmail.com'
+subject = ' this is my subject!'
+body = """
+gjhgjhgjgjhg
+"""
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_reciver
+em['Subject'] = subject
+em.set_content(body)
+
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL('smtp.gmail.com',465,context = context) as smtp
+    smtp.login(email_sender,email_password)
+    smtp.sendmail(email_sender ,email_reciver,em.as_string)
+
+
+
+
