51997b3cdde0be0da3af0acddcfbb05bbed257ba
05443 <0544354125m@gmail.com>
2022-09-03 23:09:10 +0300

    test

--- main.py
+++ main.py
@@ -20,10 +20,11 @@ em['Subject'] = subject
 em.set_content(body)
 
 
+
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL('smtp.gmail.com',465,context = context) as smtp
-    smtp.login(email_sender,email_password)
-    smtp.sendmail(email_sender ,email_reciver,em.as_string)
+smtp.login(email_sender,email_password)
+smtp.sendmail(email_sender ,email_reciver,em.as_string)
 
 
 
